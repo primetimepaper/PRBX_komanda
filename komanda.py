a7754e13e1b94099f57585734538e7fa912282d4
Pratham B <pb1028@york.ac.uk>
2023-04-13 18:31:25 +0100

    lyr nrm

--- komanda.py
+++ komanda.py
@@ -80,7 +80,7 @@ def process_csv(filename, val=5):
 test_seq = read_csv("interpolated_test.csv") # interpolated.csv for testset filled with dummy values 
 #ANCHOR - 5
 #layer_norm = lambda x: tf.compat.v1.estimator.layers.layer_norm(inputs=x, center=True, scale=True, activation_fn=None, trainable=True)
-layer_norm = lambda x: tf.keras.layers.LayerNormalization(inputs=x, center=True, scale=True, activation_fn=None, trainable=True)
+layer_norm = tf.keras.layers.LayerNormalization(center=True, scale=True, activation_fn=None, trainable=True)
 def get_optimizer(loss, lrate):
     optimizer = tf.train.AdamOptimizer(learning_rate=lrate)
     gradvars = optimizer.compute_gradients(loss)
