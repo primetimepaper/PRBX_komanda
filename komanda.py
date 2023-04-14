ea75999cf9047e019c90a0963c7b7999bb8c158d
Pratham B <pb1028@york.ac.uk>
2023-04-14 12:15:12 +0100

    file writer pt2

--- komanda.py
+++ komanda.py
@@ -202,8 +202,8 @@ with graph.as_default():
     tf.compat.v1.summary.scalar("rmse_gt", tf.math.sqrt(mse_gt))
     tf.compat.v1.summary.scalar("rmse_autoregressive", tf.math.sqrt(mse_autoregressive))    
     summaries = tf.compat.v1.summary.merge_all()
-    train_writer = tf.compat.v1.summary.FileWriter()
-    valid_writer = tf.compat.v1.summary.FileWriter()
+    train_writer = tf.compat.v1.summary.FileWriter('v3/train_summary', graph=graph)
+    valid_writer = tf.compat.v1.summary.FileWriter('v3/valid_summary', graph=graph)
     saver = tf.compat.v1.train.Saver(write_version=tf.compat.v1.train.SaverDef.V2)    
 #ANCHOR - 7
 gpu_options = tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=1.0)
